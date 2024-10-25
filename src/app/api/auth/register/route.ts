e0064e8aa1890e3764811a51383aec1e8e308c0b
tomiloki <to.escalante@duocuc.cl>
2024-10-25 01:34:01 -0300

    rolin

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,11 +1,61 @@
 import { NextRequest, NextResponse } from 'next/server';
 import db from '@/libs/db'
+import bcrypt from 'bcrypt'
 
 export async function POST(request: NextRequest) {
-    const data = await request.json();
-    
-    console.log(data);
-    const newUser = await db.user.create({data})
+    try {
+        const data = await request.json();
+        
 
-    return NextResponse.json(newUser)
+        const userFound = await db.user.findUnique({
+            where: {
+                email: data.email
+            }
+        })
+
+        if (userFound) {
+            return NextResponse.json({
+                message: 'Email already exists'
+            }, {
+                status: 400
+            })
+        }
+
+
+
+        const usernameFound = await db.user.findUnique({
+            where: {
+                username: data.username
+            }
+        })
+
+        if (usernameFound) {
+            return NextResponse.json({
+                message: 'Username already exists'
+            }, {
+                status: 400
+            })
+        }
+
+
+
+        const hashedPassword = await bcrypt.hash(data.password, 10)
+        const newUser = await db.user.create({
+            data: {
+                username: data.username,
+                email: data.email,
+                password: hashedPassword
+            }
+        })
+
+        const{password: _, ...user} = newUser
+
+        return NextResponse.json(user)
+    } catch (error: any) {
+        return NextResponse.json({
+            message: error.message
+        }, {
+            status: 500
+        })
+    }
 }
